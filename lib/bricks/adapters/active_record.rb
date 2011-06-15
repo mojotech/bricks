9d6efab44b20b87caedf75f9f14ab256cfd68739
david <dgleal@gmail.com>
2011-06-15 11:50:51 +0100

    Support all kinds of associations in ActiveRecord adapter.

--- lib/bricks/adapters/active_record.rb
+++ lib/bricks/adapters/active_record.rb
@@ -4,12 +4,32 @@ require 'active_record'
 module Bricks
   module Adapters
     module ActiveRecord
-      def association?(name)
-        @class.reflect_on_association(name.to_sym)
+      class Association
+        attr_reader :type
+
+        def initialize(klass, kind)
+          @class = klass
+          @type  = case kind
+                   when :belongs_to;                         :one
+                   when :has_many, :has_and_belongs_to_many; :many
+                   else "Unknown AR association type: #{kind}."
+                   end
+        end
+
+        def klass
+          @class
+        end
       end
 
-      def association(name)
-        @class.reflect_on_association(name.to_sym)
+      def association?(name, type = nil)
+        association(name, type)
+      end
+
+      def association(name, type = nil)
+        ar = @class.reflect_on_association(name.to_sym)
+        a  = Association.new(ar.klass, ar.macro)
+
+        a if type.nil? || a.type == type
       end
 
       Bricks::Builder.send(:include, self)
